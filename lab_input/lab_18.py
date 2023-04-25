4d6bc8108c3b3f158011b3865cbf79ff689174ae
dramdayal <dramdayal@f5.com>
2023-04-25 16:49:58 +0000

    join and split

---
+++ lab_input/lab_18.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Alta3 Research | Lab 18
+   print() - Lab 18 challenge"""
+
+def main():
+
+    # collect string input from the user
+    user_input = input("Hello, what is your name:")
+    
+    
+    #asking for day of the week
+
+    week = input("Please enter the day of the week:")
+
+    # Printing the user name and day of week
+    print("Hello:", user_input" Happy", week)
+    
+
+main()
